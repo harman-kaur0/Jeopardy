02a9e5afc25447484a4a4c214757963b9b4ff9a3
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-04-19 17:03:40 -0700

    handleClick, handleSubmit

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const url = "http://jservice.io/api/random";
 const form = document.querySelector("form");
 const button = document.querySelector("button");
 const quesForm = document.querySelector("#questionForm");
+const categories = document.querySelector("#categories");
+const h3 = document.querySelector("h3");
+const button2 = document.createElement("button");
+button2.innerText = "next Question";
+
 let randomAnswer;
 
 function initialize() {
@@ -11,28 +16,53 @@ function initialize() {
 
 function randomQuestion() {
   initialize().then((data) => {
-    // console.log(data[0]);
+    console.log(data[0].answer);
     question.innerText = data[0].question;
     randomAnswer = data[0].answer;
+    h3.innerText = `Category: ${data[0].category.title}`;
   });
 }
+//different answer scenerio
+// the Bay of Biscay
+// <i>The Fourth Musketeer</i>
+// "The Jazz Singer"
+// 15 (9 + 6)
+// 1918 (World War I)
+// (George) Luger
+
 randomQuestion();
+
+function corAns() {
+  const p = document.createElement("p");
+  p.id = "correctAnswer";
+  p.innerText = `Answer:  ${randomAnswer}`;
+  quesForm.appendChild(p);
+}
 
 function handleSubmit(e) {
   e.preventDefault();
   let value = e.target.answer.value;
   if (value === randomAnswer) {
-    alert("You got it right");
+    corAns();
+    alert("You are a genius!!!");
   } else {
-    alert("Better luck next time!");
+    corAns();
   }
+  quesForm.replaceChild(button2, form);
   e.target.reset();
-  randomQuestion();
 }
 
 function handleClick() {
   quesForm.style.display = "block";
 }
 
+function buttonClick() {
+  randomQuestion();
+  quesForm.replaceChild(form, button2);
+  const ans = document.querySelector("#correctAnswer");
+  quesForm.removeChild(ans);
+}
+
 form.addEventListener("submit", handleSubmit);
 button.addEventListener("click", handleClick);
+button2.addEventListener("click", buttonClick);
