d27b85585cdfad7731ee4fd20ec067e39673205f
Harmandeep Kaur <harmandeepk628@gmail.com>
2021-04-21 10:39:58 -0700

    added code for <i>

--- script.js
+++ script.js
@@ -9,10 +9,9 @@ const h3 = document.querySelector("h3");
 const questionValue = document.querySelector("#value");
 const button2 = document.createElement("button");
 button2.innerText = "next Question";
-const h2 = document.querySelector('h2')
-const correctSticker = document.querySelector('#correct-gif')
-const incorrectSticker = document.querySelector('#incorrect-gif')
-
+const h2 = document.querySelector("h2");
+const correctSticker = document.querySelector("#correct-gif");
+const incorrectSticker = document.querySelector("#incorrect-gif");
 
 let randomAnswer;
 let currentQuesValue;
@@ -33,18 +32,17 @@ function randomQuestion() {
     }
 
     randomAnswer = data[0].answer;
-    if (randomAnswer.charAt(0) === '<') {
-      randomAnswer = randomAnswer.slice(3, -4)
+    if (randomAnswer.charAt(0) === "<") {
+      randomAnswer = randomAnswer.slice(3, -4);
     }
 
     currentQuesValue = data[0].value;
-    console.log(randomAnswer)
+    console.log(randomAnswer);
     questionValue.innerText = `Value: ${parseInt(data[0].value)}`;
     // console.log(parseInt(data[0].value));
     h3.innerText = `Category: ${data[0].category.title}`;
   });
 }
-
 
 randomQuestion();
 
@@ -64,16 +62,15 @@ function handleSubmit(e) {
   e.preventDefault();
   let value = e.target.answer.value;
   if (equalAnswers(randomAnswer) === equalAnswers(value)) {
-    
     corAns();
     h4.innerText = parseInt(h4.innerText) + parseInt(currentQuesValue);
-    correctSticker.style.display = 'block'
-    setTimeout(()=>correctSticker.style.display = "none", 3000)
+    correctSticker.style.display = "block";
+    setTimeout(() => (correctSticker.style.display = "none"), 3000);
     // console.log(h4.innerText);
   } else {
     corAns();
-    incorrectSticker.style.display = 'block'
-    setTimeout(()=>incorrectSticker.style.display = "none", 3000)
+    incorrectSticker.style.display = "block";
+    setTimeout(() => (incorrectSticker.style.display = "none"), 3000);
   }
   quesForm.replaceChild(button2, form);
   e.target.reset();
@@ -96,30 +93,31 @@ function handleSubmit(e) {
 
 // puts user answer and correct answer in same format
 function equalAnswers(ans) {
-  ans = ans.toLowerCase()
-  if (ans.charAt(0) === '<') {
-    ans = ans.slice(3, -4)
-  } else if (ans.indexOf('(')!== -1) {
-    ans = ans.split('')
-    const openParen = ans.indexOf('(')
-    const closeParen = ans.indexOf(')')
-    ans.splice(openParen, closeParen - openParen + 1)
-    ans = ans.join('').trim()
+  ans = ans.toLowerCase();
+  if (ans.includes("<i>")) {
+    ans = ans.replaceAll("<i>", "");
+    ans = ans.replaceAll("</i>", "");
+  } else if (ans.indexOf("(") !== -1) {
+    ans = ans.split("");
+    const openParen = ans.indexOf("(");
+    const closeParen = ans.indexOf(")");
+    ans.splice(openParen, closeParen - openParen + 1);
+    ans = ans.join("").trim();
   }
-  ans = ans.split('')
-  ans = ans.filter(char => char.match(/[A-Za-z0-9&.,-]|\s|\'/g))
-  ans = ans.join('')
-  console.log(ans)
-  return ans
+  ans = ans.split("");
+  ans = ans.filter((char) => char.match(/[A-Za-z0-9&.,-]|\s|\'/g));
+  ans = ans.join("");
+  console.log(ans);
+  return ans;
 }
 
 // starts the game
 function handleClick() {
   quesForm.style.display = "block";
-  h4.style.display = 'block';
-  h2.style.display = 'block';
-  button.style.display = 'none';
-  startButton.style.display = 'block';
+  h4.style.display = "block";
+  h2.style.display = "block";
+  button.style.display = "none";
+  startButton.style.display = "block";
 }
 
 // next question button
